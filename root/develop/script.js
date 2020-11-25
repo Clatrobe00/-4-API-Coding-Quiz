38c08dd182ea713152c7485a14ea91c986620e11
Charles Latrobe <Charlie.bateman117@gmail.com>
2020-11-25 13:54:44 -0800

    add pseudocode for js function layout

---
+++ root/develop/script.js
@@ -0,0 +1,16 @@
+var startButton = document.getElementById("startButton");
+startButton.addEventListener('click', beginQuiz); 
+    
+function beginQuiz(startButton) {
+    console.log("click");
+    //beginTimer();
+    //showQuiz()
+}    
+    
+function beginTimer() {
+    //this will be a setInterval function that starts time and invokes and time-out ending function
+}
+
+function showQuiz() {
+    //this function will control what is displayed for each question
+}
